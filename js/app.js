6e8e8afb9248b0a6e97ce3e9c59e93a02406bad1
ghchoi0427 <ghchoi0427@gmail.com>
2022-04-04 23:45:01 +0900

    feat: createUser 생성

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 
-var isRunning = false;
-
-function startAndPause(){
-    if(pause){
-        
+class User {
+    constructor(id, name, group, subjects, friends, records) {
+        this.id = id;
+        this.name = name;
+        this.group = group;
+        this.subjects = subjects;
+        this.friends = friends;
+        this.records = records;
     }
+}
+
+function creatUser(id, name, group){
+    var user = new User();
+    user.id = id;
+    user.name = name;
+    user.group = group;
+    return user;
 }
