660abf3a0009f247c44fc56422d89691edcc3009
ghchoi0427 <ghchoi0427@gmail.com>
2022-04-09 11:37:24 +0900

    feat: 회원가입

--- server/src/main/java/com/cgh/server/controller/LoginController.java
+++ server/src/main/java/com/cgh/server/controller/LoginController.java
@@ -1,20 +1,37 @@
 package com.cgh.server.controller;
 
+import com.cgh.server.domain.User;
+import com.cgh.server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("")
-    public String login(){
+    public String login() {
         return "login";
     }
 
     @GetMapping("/signup")
-    public String signup(){
+    public String signup(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
         return "signup";
     }
+
+    @PostMapping("/signup")
+    public String signup(User user) {
+        userService.save(user);
+        return "redirect:/login";
+    }
+
 }
