ae5f792b1baff597310e9307f0d4fea438f865d6
ghchoi0427 <ghchoi0427@gmail.com>
2022-04-09 11:40:47 +0900

    feat: User 서비스 생성

---
+++ server/src/main/java/com/cgh/server/service/UserService.java
@@ -0,0 +1,35 @@
+package com.cgh.server.service;
+
+import com.cgh.server.domain.User;
+import com.cgh.server.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return new ArrayList<>(userRepository.findAll());
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
