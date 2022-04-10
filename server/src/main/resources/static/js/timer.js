e95444526fce4faa3fa4648789a53d48eb62871e
ghchoi0427 <ghchoi0427@gmail.com>
2022-04-10 12:51:23 +0900

    feat: timer.js 생성

---
+++ server/src/main/resources/static/js/timer.js
@@ -0,0 +1,32 @@
+var pause = false;
+var count = 0;
+
+countTimers();
+
+function countTimers() {
+
+    setInterval(timer, 1000);
+
+    function timer() {
+        if (!pause) {
+            count = count + 1;
+
+            var hours = Math.floor((count % (60 * 60 * 24)) / (60 * 60));
+            var minutes = Math.floor((count % (60 * 60)) / 60);
+            var seconds = Math.floor(count % 60);
+
+            document.getElementById("timer").innerHTML = hours + ": "
+                + minutes + ": " + seconds;
+
+        }
+    }
+
+}
+
+function startTimer() {
+    pause = false;
+}
+
+function pauseTimer() {
+    pause = true;
+}
