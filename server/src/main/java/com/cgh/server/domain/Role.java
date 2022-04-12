3b5dc96a129dd8e88e9e5ecb6d205a445dd9542d
ghchoi0427 <ghchoi0427@gmail.com>
2022-04-12 13:48:56 +0900

    feat: enum Role 추가

---
+++ server/src/main/java/com/cgh/server/domain/Role.java
@@ -0,0 +1,11 @@
+package com.cgh.server.domain;
+
+public enum Role {
+    ROLE_ADMIN("관리자"), ROLE_MANAGER("매니저"), ROLE_MEMBER("일반사용자");
+
+    private String description;
+
+    Role(String description) {
+        this.description = description;
+    }
+}
