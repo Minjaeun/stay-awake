0535151f781c3d1793e23bace9ac6ebcf3a860bd
ghchoi0427 <ghchoi0427@gmail.com>
2022-04-12 13:45:32 +0900

    fix: Spring Security 적용

--- server/src/main/java/com/cgh/server/controller/LoginController.java
+++ server/src/main/java/com/cgh/server/controller/LoginController.java
@@ -1,14 +1,22 @@
 package com.cgh.server.controller;
 
-import com.cgh.server.domain.User;
+import com.cgh.server.domain.Member;
+import com.cgh.server.domain.Role;
+import com.cgh.server.domain.dto.LoginRequest;
+import com.cgh.server.domain.dto.RegistryRequest;
 import com.cgh.server.service.LoginService;
 import com.cgh.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/login")
@@ -18,34 +26,37 @@ public class LoginController {
     LoginService loginService;
     @Autowired
     UserService userService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public LoginController() {
+        passwordEncoder = new BCryptPasswordEncoder();
+    }
 
     @GetMapping("")
     public String login(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        return "login";
-    }
-
-    @PostMapping("")
-    public String login(User user) {
-        if (loginService.existsByNameAndPassword(user.getName(), user.getPassword())) {
-            return "redirect:/home";
-        } else {
-            return "redirect:/login";
-        }
+        model.addAttribute("member", new LoginRequest());
+        return "/login";
     }
 
     @GetMapping("/signup")
     public String signup(Model model) {
-        User user = new User();
-        model.addAttribute("user", user);
+        model.addAttribute("member", new RegistryRequest());
         return "signup";
     }
 
     @PostMapping("/signup")
-    public String signup(User user) {
-        userService.save(user);
+    public String signup(@ModelAttribute RegistryRequest registryRequest) {
+        Member member = new Member(registryRequest.getUsername(), passwordEncoder.encode(registryRequest.getPassword()));
+        userService.save(member);
         return "redirect:/login";
     }
 
+    @ModelAttribute("roles")
+    public Map<String, Role> roles() {
+        Map<String, Role> map = new LinkedHashMap<>();
+        map.put("관리자", Role.ROLE_ADMIN);
+        map.put("매니저", Role.ROLE_MANAGER);
+        map.put("일반 사용자", Role.ROLE_MEMBER);
+        return map;
+    }
 }
