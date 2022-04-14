cb658c2dcc7d4fd306c05c9ceda231551d5845fa
ghchoi0427 <ghchoi0427@gmail.com>
2022-04-14 15:01:47 +0900

    feat: 현재 로그인한 사용자 나타내기

--- server/src/main/java/com/cgh/server/controller/HomeController.java
+++ server/src/main/java/com/cgh/server/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.cgh.server.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @GetMapping("")
-    public String home() {
+    public String home(Model model, @AuthenticationPrincipal User user) {
+        if (user != null) {
+            model.addAttribute("currentUser", user);
+        }
         return "home";
     }
 }
