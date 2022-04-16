1eae5b71c243cb8bf733c9f21f95ff074641f2e3
ghchoi0427 <ghchoi0427@gmail.com>
2022-04-16 21:16:38 +0900

    feat: TeamDto 생성

---
+++ server/src/main/java/com/cgh/server/domain/dto/TeamDto.java
@@ -0,0 +1,14 @@
+package com.cgh.server.domain.dto;
+
+public class TeamDto {
+
+    String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
